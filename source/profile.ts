1a60f640cc58a3a797db1c76aab5b6419527f988
Alejandro Muñoz Del Álamo <Urielkaralden@gmail.com>
2022-10-12 10:53:58 +0200

    Added IHasFormatter

--- source/profile.ts
+++ source/profile.ts
@@ -1,3 +1,6 @@
+import { IHasFormatter } from './classes/interfaces/IHasFormatter';
+import { Milestone } from './classes/Milestone.ts';
 
-
+let firstMilestone = new Milestone ("First Milestone", "My first milestone", "1990-10-24", "1990-10-24");
+firstMilestone.format();
 
